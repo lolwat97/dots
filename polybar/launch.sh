6e5c565ef4cb239c1bb1c599a932e2ea31b1ec54
lw97 <lolwat97@gmail.com>
2022-07-27 05:05:36 +0300

    a lot of work done on polybar, can't wait to fuck it up with colors

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -3,4 +3,6 @@
 killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-polybar mybar 2>&1 | tee -a /home/lw/.config/polybar/polybar1.log & disown
+polybar i3bar 2>&1 | tee -a /home/lw/.config/polybar/i3bar.log & disown
+polybar traybar 2>&1 | tee -a /home/lw/.config/polybar/traybar.log & disown
+polybar centerbar 2>&1 | tee -a /home/lw/.config/polybar/centerbar.log & disown
